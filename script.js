dd0b537db993d63585dfd1916dbff9d1a1a88c01
wwr <wangwr17@163.com>
2023-12-02 17:18:09 -0500

    update name

--- script.js
+++ script.js
@@ -9,69 +9,32 @@ var GENESIS = '0x000000000000000000000000000000000000000000000000000000000000000
 
 // This is the ABI for your contract (get it from Remix, in the 'Compile' tab)
 // ============================================================
-var abi = []; // FIXME: fill this in with your contract's ABI //Be sure to only have one array, not two
+var abi = []; // FIXME: fill this in with your contract's ABI 
 
 // ============================================================
 abiDecoder.addABI(abi);
 // call abiDecoder.decodeMethod to use this - see 'getAllFunctionCalls' for more
 
-var contractAddress = ''; // FIXME: fill this in with your contract's address/hash
+var contractAddress = '0x0000000000000000000000000000000000000000'; // FIXME: fill this in with your contract's address/hash
 var ContractGuard = new web3.eth.Contract(abi, contractAddress);
 
-async function getAllContractData(){
-    var function_calls = await getAllFunctionCalls(contractAddress, 'addContract');
-    var nodes = [];
-    var links = [];
+// TODO: add an IOU ('I owe you') to the system
+// The person you owe money is passed as 'creditor'
+// The amount you owe them is passed as 'amount'
+async function add_Contract(user1add, user2add, name1, name2, credit, content) {
 
-    //get all nodes
-    var nodeSet = new Set();
-    for (var i = 0; i < function_calls.length; i++) {
-        var args = function_calls[i].args;
-        // (name: name of the user, address: address of the user)
-        nodeSet.add({name: args[2], address: args[0]});
-        nodeSet.add({name: args[3], address: args[1]});
-        // (source: the address of user1, target: the address of user2, value: the credit)
-        links.push({source: args[0], target: args[1], value: args[4]});
-    }
-    //convert set to array
-    nodes = Array.from(nodeSet);
-    //return nodes and links
-    return {nodes: nodes, links: links};
+	await ContractGuard.methods.AddContract(user1add, user2add, name1, name2, credit, content).send(
+		{from: web3.eth.defaultAccount, gas: 3000000}
+	)
 
 }
 
-
-async function getAllFunctionCalls(addressOfContract, functionName) {
-
-	var curBlock = await web3.eth.getBlockNumber();
-	var function_calls = [];
-
-	while (curBlock !== GENESIS) {
-	  var b = await web3.eth.getBlock(curBlock, true);
-	  var txns = b.transactions;
-	  for (var j = 0; j < txns.length; j++) {
-	  	var txn = txns[j];
-
-	  	// check that destination of txn is our contract
-			if(txn.to == null){continue;}
-	  	if (txn.to.toLowerCase() === addressOfContract.toLowerCase()) {
-	  		var func_call = abiDecoder.decodeMethod(txn.input);
-				//todo: check that the tx is successful
-				// check that the function getting called in this txn is 'functionName'
-				if (func_call && func_call.name === functionName) {
-					var time = await web3.eth.getBlock(curBlock);
-	  			var args = func_call.params.map(function (x) {return x.value});
-	  			function_calls.push({
-	  				from: txn.from.toLowerCase(),
-	  				args: args,
-						t: time.timestamp
-	  			})
-	  		}
-	  	}
-	  }
-	  curBlock = b.parentHash;
-	}
-	return function_calls;
-}
-
-console.log(getAllContractData());
+// UI
+// This runs the 'add_IOU' function when you click the button
+// It passes the values from the two inputs above
+$("#addcontract").click(function() {
+	// web3.eth.defaultAccount = $("#myaccount").val(); //sets the default account
+    add_Contract($("#creditor").val(), $("#amount").val()).then((response)=>{
+		window.location.reload(true); // refreshes the page after add_IOU returns and the promise is unwrapped
+	})
+});
